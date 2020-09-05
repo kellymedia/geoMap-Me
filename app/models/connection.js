bfd818f965ce78efe140546390774d717ab4a6c4
kellymedia <mk@uconn.edu>
2020-09-05 12:51:29 -0400

    fix Heroku deployment

---
+++ app/models/connection.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "geoMap" model that matches up with DB
+var MapMe = sequelize.define("mapMe", {
+  author: Sequelize.STRING,
+  body: Sequelize.STRING,
+  latitude: Sequelize.FLOAT,
+  longitude: Sequelize.FLOAT,
+  created_at: Sequelize.DATE
+});
+
+// Syncs with DB
+MapMe.sync();
+
+// Makes the Marker Model available for other files (will also create a table)
+module.exports = MapMe;
